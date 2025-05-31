9ac8570e3214468a3711fe7af914be25e9c15b9e
Skr021002 <skr021002@gmail.com>
2025-05-31 16:14:50 +0530

    Update Guess_The_Number.py

--- Guess_The_Number.py
+++ Guess_The_Number.py
@@ -1,9 +1,10 @@
 import random
-number_to_guess=random.randint(1,100)
-while True:
-    try:
+number_to_guess=random.randint(1,100) #import random and it will pick a number between 1 to 100 
+
+while True: #add a while such that the loop continue until user input match the random
+    try: #try if the user enter values like characters,etc... to except it 
         guess=int(input("Guess the number between 1 to 100\n"))
-        if guess < number_to_guess:
+        if guess < number_to_guess: 
             print("Too low!")
         elif guess > number_to_guess:
             print("Too high!")
